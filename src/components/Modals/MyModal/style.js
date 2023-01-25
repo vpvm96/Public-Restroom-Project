bb28ab9a5204d4b8c3e2d007c026f6a8dc35ed39
mr-chacha <hoitchac@gmail.com>
2023-01-25 21:08:46 +0900

    fix 수정중

--- src/components/Modals/MyModal/style.js
+++ src/components/Modals/MyModal/style.js
@@ -1,23 +1,36 @@
 import styled from 'styled-components';
 
 const ModalLayout = styled.div`
-  width: 800px;
+  width: 60%;
   height: 100%;
-  padding: 10px 10px 10px 10px;
+  padding: 10px 0px 10px 0px;
 `;
 
 const ModalBox = styled.div``;
 const MyModal = styled.div`
-  width: 600px;
+  width: 100%;
   height: 150px;
+  margin-top: 10px;
 `;
 const ModalInput = styled.input`
   width: 100%;
-  height: 100px;
+  height: 80px;
   padding: 10px;
   padding-bottom: 70px;
   margin-top: 10px;
   margin-bottom: 10px;
 `;
 
-export { ModalLayout, ModalBox, ModalInput, MyModal };
+const ModalSapan = styled.span`
+  padding: 5px;
+  margin-left: 200px;
+  width: 100%;
+  height: 100%;
+  border: 1px solid black;
+  background-color: blue;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export { ModalLayout, ModalBox, ModalInput, MyModal, ModalSapan };
--- src/components/Modals/YourModal/style.js
+++ src/components/Modals/YourModal/style.js
@@ -1,23 +1,50 @@
 import styled from 'styled-components';
 
+const YourModlasText = styled.div`
+  position: absolute;
+`;
 const YourModals = styled.div`
   margin-top: 30px;
-  padding: 10px;
-  width: 600px;
+  width: 100%;
   height: 100px;
   border: 1px solid black;
   word-wrap: break-word;
+  padding: 10px;
   display: flex;
+  position: relative;
 `;
-const YourModalsBtnArea = styled.div``;
+const YourModalsBtnArea = styled.div`
+  width: 100px;
+  height: 30px;
+  position: absolute;
+  right: 0px;
+`;
 const YourModalsLayout = styled.div`
-  width: 800px;
+  width: 100%;
   height: 100%;
-  padding: 10px 10px 10px 10px;
 `;
 const YourModalsInput = styled.input`
   width: 100%;
   height: 100%;
-  display: none;
+  margin: 0px;
 `;
-export { YourModals, YourModalsBtnArea, YourModalsLayout, YourModalsInput };
+const YourModalSapan = styled.span`
+  padding-left: 50px;
+  padding: 5px;
+  margin-left: 2px;
+  width: 100%;
+  height: 100%;
+  border: 1px solid black;
+  background-color: blue;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+export {
+  YourModalSapan,
+  YourModals,
+  YourModalsBtnArea,
+  YourModalsLayout,
+  YourModalsInput,
+  YourModlasText,
+};

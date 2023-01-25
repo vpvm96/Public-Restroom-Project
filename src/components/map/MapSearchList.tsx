ece3032bba0ea5906b6f7c83f90ba4d6052a2823
mr-chacha <hoitchac@gmail.com>
2023-01-25 16:08:20 +0900

    수정중

--- src/components/map/MapSearchList.tsx
+++ src/components/map/MapSearchList.tsx
@@ -20,7 +20,7 @@ const MapSearchList = ({ markerInfo }: MapSearchListProps) => {
           </MapSearchListLeftAdressText>
         </MapSearchListLeftBox>
         <MapSearchListRightBox>
-          <Modal />
+          <Modal GU_NM={GU_NM} HNR_NAM={HNR_NAM} />
         </MapSearchListRightBox>
       </MapSearchListContainer>
     </MapSearchListWrapper>

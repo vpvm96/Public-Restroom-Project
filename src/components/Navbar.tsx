2701ed9e98678af590f82bad078c928f2421ccb9
Wonseok <dnjstjr970302@gamil.com>
2023-01-26 16:16:34 +0900

    fix:네브바 구조 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import logoImg from '../assets/await.png';
-import profileImg from '../assets/cat.jpg';
-import blankImg from '../assets/blank.png';
-import DfProfileImg from '../assets/profile.png';
-import { getAuth } from 'firebase/auth';
-
 import { signOut } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { authService } from '../api/firebaseService';
 import { useNavigate } from 'react-router-dom';
 import useLoginState from '../hooks/useLoginState';
+import blankImg from '../assets/blank.png';
+import DfProfileImg from '../assets/profile.png';
+import profileImg from '../assets/cat.jpg';
+import logoImg from '../assets/await.png';
+import styled from 'styled-components';
 
 const Navbar = () => {
   const auth = getAuth();
@@ -35,35 +34,47 @@ const Navbar = () => {
           <LogoImg src={logoImg} alt="Logo" />
         </Link>
       </LeftSection>
-      <NavUl>
-        <NavLi>
-          <NavText to="/">Home</NavText>
-        </NavLi>
-        <NavLi>
-          <NavText to="/map">Map Page</NavText>
-        </NavLi>
-        <NavLi>
-          <NavText to="/mypage">My Page</NavText>
-        </NavLi>
-      </NavUl>
-      <div>
-        {auth.currentUser?.photoURL ? (
-          <ProfileImg src={auth.currentUser.photoURL} />
-        ) : (
-          <ProfileImg src={DfProfileImg} />
-        )}
-
-        <div>{auth.currentUser?.displayName} </div>
-      </div>
-
-      {/* <LoginButtonBox to="/login"> */}
-      <LoginButtonBox>
+      <MenuSection>
+        <NavUl>
+          <NavLi>
+            <NavText to="/">Home</NavText>
+          </NavLi>
+          <NavLi>
+            <NavText to="/map">Map Page</NavText>
+          </NavLi>
+          <NavLi>
+            <NavText to="/mypage">My Page</NavText>
+          </NavLi>
+        </NavUl>
+      </MenuSection>
+      <InfoSection>
         {isLoggedIn && isAuthorizedInSession ? (
-          <LoginButton onClick={handleLogout}>Logout</LoginButton> //React.MouseEventHandler<HTMLButtonElement>
+          <ImgNick>
+            <FbImg>
+              {auth.currentUser?.photoURL ? (
+                <ProfileImg src={auth.currentUser.photoURL} />
+              ) : (
+                <ProfileImg src={DfProfileImg} />
+              )}
+            </FbImg>
+            <FontBox>
+              <Font>{auth.currentUser?.displayName} </Font>
+            </FontBox>
+          </ImgNick>
         ) : (
-          <LoginButton onClick={() => navigate('/login')}>Login</LoginButton>
+          <FontBox>
+            <Font>{auth.currentUser?.displayName} </Font>
+          </FontBox>
         )}
-      </LoginButtonBox>
+        {/* <LoginButtonBox to="/login"> */}
+        <LoginButtonBox>
+          {isLoggedIn && isAuthorizedInSession ? (
+            <LoginButton onClick={handleLogout}>Logout</LoginButton> //React.MouseEventHandler<HTMLButtonElement>
+          ) : (
+            <LoginButton onClick={() => navigate('/login')}>Login</LoginButton>
+          )}
+        </LoginButtonBox>
+      </InfoSection>
     </Nav>
   );
 };
@@ -71,32 +82,58 @@ const Navbar = () => {
 export default Navbar;
 
 const Nav = styled.div`
+  width: 100%;
+  height: 8vh;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  /* background-color: #f9f9fb; */
-  background-color: #ffff;
-  padding: 30px 35px 30px 35px;
+  background-color: #f9f9fb;
 `;
 
 const LeftSection = styled.div`
-  justify-items: flex-start;
-  display: flex;
-  margin: 0px;
+  width: 15%;
+  margin-left: 4rem;
 `;
 
 const LogoImg = styled.img`
   width: 150px;
 `;
 
+const ImgNick = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-items: center;
+  text-align: center;
+  gap: 1rem;
+`;
+
+const FbImg = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FontBox = styled.div`
+  width: 100%;
+`;
+
+const Font = styled.p`
+  font-weight: 500;
+  font-size: 35px;
+  font-style: oblique;
+  margin-bottom: 0.5rem;
+`;
+
+const MenuSection = styled.div`
+  width: 45%;
+`;
+
 const NavUl = styled.ul`
   display: flex;
-  /* width: 40%; */
+  flex-direction: row;
 `;
 
 const NavLi = styled.li`
   list-style: none;
-  margin-right: 100px;
   text-decoration: none;
 `;
 const NavText = styled(Link)`
@@ -104,6 +141,15 @@ const NavText = styled(Link)`
   color: black;
   font-size: 20px;
 `;
+
+const InfoSection = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
 const LoginButton = styled.button`
   border: none;
   padding-left: 25px;
@@ -117,15 +163,13 @@ const LoginButton = styled.button`
   color: white;
   cursor: pointer;
 `;
-
-// const LoginButtonBox = styled(Link)`
-//   margin: 0;
-// `;
-
-const LoginButtonBox = styled.div``;
+const LoginButtonBox = styled.div`
+  margin-right: 1.5rem;
+  align-items: flex-end;
+`;
 
 const ProfileImg = styled.img`
-  width: 40px;
-  height: 40px;
+  width: 45px;
+  height: 45px;
   border-radius: 100%;
 `;

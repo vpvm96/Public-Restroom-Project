07fced4f9d4473db2787059b9ca399b62ccc823d
Wonseok <dnjstjr970302@gamil.com>
2023-01-28 13:12:52 +0900

    ..

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -78,7 +78,6 @@ const MainPage = () => {
               <p style={{ position: 'absolute', top: '25%' }}>{r.title}</p>
               <p style={{ position: 'absolute', top: '45%' }}>{r.content}</p>
               <p style={{ position: 'absolute', top: '80%', right: '5%' }}>
-                {/* {r.createdAt.toString().slice(0, 24)} */}
                 {r.createdAt}
               </p>
             </ReviewBox>
